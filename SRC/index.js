28bed5dbd64655e5229eb8d73d445935c1cd0e2f
Nawane Mançano <138410185+nawanemacano@users.noreply.github.com>
2024-07-09 09:48:19 -0300

    Add files via upload

--- SRC/index.js
+++ SRC/index.js
@@ -1,1 +1,24 @@
+import openDB from './configDB.js';
+import express from 'express';
+import { createTable } from './controller/tarefa.js'
+
+openDB();
+const app = express();
+app.use(express.json());
+
+createTable();
+
+app. get('/', (req, res) =>{
+    res.send("API RESTful rodando com node.js");
+
+})
+
+app.post('/tarefa1', (req, res) =>{
+    console.log(req.body);
+    res.json({
+        "statuscode": 200
+    })
+})
+
+app.listen(3000, ()=>console.log("API RODANDO"));
 
